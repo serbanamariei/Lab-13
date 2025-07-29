79f51b8c37655d7e8819327db014781e552276fb
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 20:54:04 +0300

    Create functii.h

---
+++ problema-3.1/functii.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+void citireVector(FILE *f, int v[], int n);
+void afisareVector(FILE *f, int v[], int n);
+int valMax(int v[], int n);
+int valMin(int v[], int n);
+double mediaAritmetica(int v[], int n);
+double mediaGeometrica(int v[], int n);
+
+#endif
