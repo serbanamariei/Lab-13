463a939cb4ddb35ee64eaf814bccdd517bb092fe
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 20:57:48 +0300

    Create functii.h

---
+++ problema-4.3/functii.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+void citire(FILE *f, int v[][22], int n, int m);
+void afisare(int v[][22], int n, int m);
+void parceleVarf(int v[][22], int n, int m);
+
+#endif
