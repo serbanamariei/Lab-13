e53f32f76eb7eb134c4abb5a152cf1ea253baa89
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 20:57:04 +0300

    Create functii.h

---
+++ problema-4.2/functii.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+void citire(FILE *f, double v[][20], int n, int m);
+void afisare(double v[][20], int n, int m);
+double normaInfinit(double v[][20], int n, int m);
+double norma1(double v[][20], int n, int m);
+double norma2(double v[][20], int n, int m);
+
+#endif
