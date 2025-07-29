96e2de23f071943780aa922774f520454a59539c
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 20:55:44 +0300

    Create functii.h

---
+++ problema-3.3/functii.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+void citire(FILE *f, int v[], int n);
+void plata(double pl[], int com[][20], int pret[], int n, int m);
+void afisare(FILE *f, int v[], int n);
+void munca(FILE *f, double pl[], int n, int plata);
+
+#endif
